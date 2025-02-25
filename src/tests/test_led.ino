8a032f6e8db36aba334bb11d13fe0016db5c0832
Edney10 <aluno26770@mtorga.edu.pt>
2025-02-25 12:31:14 +0000

    Create test_led.ino

---
+++ src/tests/test_led.ino
@@ -0,0 +1,16 @@
+/*
+*  Teste do LED
+*/
+
+#define LED_PIN 11
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+}
